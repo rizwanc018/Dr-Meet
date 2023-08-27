1d8566da4ab5cfe664e3bf56431c521c6dfb062a
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-08-27 10:26:52 +0530

    server config changed

--- backend/routes/stripe.js
+++ backend/routes/stripe.js
@@ -21,7 +21,7 @@ const stripe = Stripe(process.env.STRIPE_API_KEY)
 //    stripe listen --forward-to localhost:5000/api/stripe/webhook
 router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
 // router.post('/webhook', express.raw({ type: '*/*' }), async (req, res) => {
-    const rawBody = await getRawBody(req);
+    const rawBody = await getRawBody(req.body);
     console.log("🚀 ~ file: stripe.js:25 ~ //router.post ~ rawBody:", rawBody)
     let signinsecret = 'whsec_ef2dfc5887f870636fe513da6ef308b0c2f9b58764289374fa74f1cb4ea58f80'
     const sig = req.headers['stripe-signature'];
@@ -34,6 +34,7 @@ router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req
 
     try {
         event = await stripe.webhooks.constructEvent(rawBody, sig, signinsecret);
+        console.log("🚀 ~ file: stripe.js:37 ~ //router.post ~ event:", event)
         data = event.data.object
     } catch (err) {
         console.log(err)
