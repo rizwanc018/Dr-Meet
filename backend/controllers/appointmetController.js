d1797ebf02533e980b7c6078e498b7c408b97536
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-08-31 17:02:14 +0530

    date update

--- backend/controllers/appointmetController.js
+++ backend/controllers/appointmetController.js
@@ -7,10 +7,10 @@ const appointmentController = {
     getAppointmentsByDate: asyncHandler(async (req, res) => {
         const docId = req.doctor._id
         let { date } = req.body
-        console.log("🚀 ~ file: appointmetController.js:11 ~ getAppointmentsByDate:asyncHandler ~ moment(date).startOf('day').toISOString():", moment(date).add(1, 'day').startOf('day').toISOString())
-        console.log("🚀 ~ file: appointmetController.js:12 ~ getAppointmentsByDate:asyncHandler ~ moment(date).startOf('day').utc():", moment(date).startOf('day').utc())
+        console.log("🚀 moment(date).startOf('day').toISOString():", moment(date).startOf('day').toISOString())
         date = moment(date).add(1, 'day').startOf('day').toISOString()
-        const appointments = await Appointment.find({ docId, date , finished: false })
+        console.log("🚀 file: appointmetController.js:13 ~ getAppointmentsByDate:asyncHandler ~ date:", date)
+        const appointments = await Appointment.find({ docId, date, finished: false })
             .populate('patientId', 'fname lname mobile')
             .populate('timeId', 'startTime endTime')
             .sort({ startTime: 1 })
